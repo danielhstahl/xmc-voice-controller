ad01712aed9bff202023dbd0396d97023cb7c555
Daniel Stahl <danstahl1138@gmail.com>
2022-10-28 13:38:48 -0500

    updating to add logging

--- index.js
+++ index.js
@@ -9,33 +9,45 @@ const switchCommand = process.env.NODE_ENV === "test" ? {
     "TurnOff": () => Promise.reject(),
     "SelectInput": () => Promise.resolve("hi"),
 } : {
-    "AdjustVolume": async value => {
+    "AdjustVolume": async (node, value) => {
+
         const { volume: volumePercentChange } = value
         const volumeChange = volumePercentChange > 0 ? 5 : -5
+        node.log(`Changing volume by ${volumeChange}`);
         const { volume } = await tiny.get(`${URL}/info`)
         await tiny.post({ url: `${URL}/volume/${volume + volumeChange}` })
     },
-    "TurnOn": async () => await tiny.post({ url: `${URL}/power/on` }),
-    "TurnOff": async () => await tiny.post({ url: `${URL}/power/off` }),
-    "SelectInput": async value => {
+    "TurnOn": async (node) => {
+        node.log("Powering on")
+        await tiny.post({ url: `${URL}/power/on` })
+    },
+    "TurnOff": async (node) => {
+        node.log("Powering off")
+        await tiny.post({ url: `${URL}/power/off` })
+    },
+    "SelectInput": async (node, value) => {
         const { input: rawInput } = value
         if (rawInput.startsWith("HDMI")) {
             const [_, input] = rawInput.split(" ")
+            node.log(`Changing input to ${rawInput}`);
             await tiny.post({ url: `${URL}/hdmi/${input}` })
         }
     }
 }
 
-const parseJson = (msg, switchCommand) => {
-    return switchCommand[msg.payload.directive](msg.payload.rawDirective.directive.payload)
+const parseJson = (msg, node, switchCommand) => {
+    return switchCommand[msg.payload.directive](node, msg.payload.rawDirective.directive.payload)
 }
 
 module.exports = function (RED) {
     function ParseCommandsToXMC(config) {
         RED.nodes.createNode(this, config);
         const node = this;
+        node.log(`Url is ${URL}`)
+        node.log(`Enviroment is ${process.env.NODE_ENV}`)
         node.on('input', function (msg) {
-            parseJson(msg, switchCommand).then(r => {
+            node.log("Message received, processing");
+            parseJson(msg, node, switchCommand).then(r => {
                 msg.payload = r
                 node.send(msg);
             })
