1226ca9d630a47d26863686e3c1e7dc6159718c2
Daniel Stahl <danstahl1138@gmail.com>
2022-10-28 15:42:17 -0500

    fixed html and host

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const tiny = require('tiny-json-http')
 
 //in prod, run on same machine as rest api
-const URL = process.env.NODE_ENV === "production" ? "http://127.0.0.1:8000" : "http://moode.local:8000"
-const switchCommand = process.env.NODE_ENV === "test" ? {
+//const URL = process.env.NODE_ENV === "production" ? "http://127.0.0.1:8000" : "http://moode.local:8000"
+const switchCommand = (host) => process.env.NODE_ENV === "test" ? {
     "AdjustVolume": () => Promise.reject(),
     "TurnOn": () => Promise.reject(),
     "TurnOff": () => Promise.reject(),
@@ -13,8 +13,8 @@ const switchCommand = process.env.NODE_ENV === "test" ? {
         const volumeChange = volumePercentChange > 0 ? 5 : -5
         node.log(`Changing volume by ${volumeChange}`);
         try {
-            const { volume } = await tiny.get(`${URL}/info`)
-            await tiny.post({ url: `${URL}/volume/${volume + volumeChange}` })
+            const { volume } = await tiny.get({ url: `${host}/info` })
+            await tiny.post({ url: `${host}/volume/${volume + volumeChange}` })
         }
         catch (e) {
             node.error(e)
@@ -23,7 +23,7 @@ const switchCommand = process.env.NODE_ENV === "test" ? {
     "TurnOn": async (node) => {
         node.log("Powering on")
         try {
-            await tiny.post({ url: `${URL}/power/on` })
+            await tiny.post({ url: `${host}/power/on` })
         }
         catch (e) {
             node.error(e)
@@ -32,7 +32,7 @@ const switchCommand = process.env.NODE_ENV === "test" ? {
     "TurnOff": async (node) => {
         node.log("Powering off")
         try {
-            await tiny.post({ url: `${URL}/power/off` })
+            await tiny.post({ url: `${host}/power/off` })
         }
         catch (e) {
             node.error(e)
@@ -44,7 +44,7 @@ const switchCommand = process.env.NODE_ENV === "test" ? {
         if (rawInput.startsWith("HDMI")) {
             const [_, input] = rawInput.split(" ")
             try {
-                await tiny.post({ url: `${URL}/hdmi/${input}` })
+                await tiny.post({ url: `${host}/hdmi/${input}` })
             } catch (e) {
                 node.error(e)
             }
@@ -60,11 +60,13 @@ module.exports = function (RED) {
     function ParseCommandsToXMC(config) {
         RED.nodes.createNode(this, config);
         const node = this;
-        node.log(`Url is ${URL}`)
-        node.log(`Enviroment is ${process.env.NODE_ENV}`)
+        const host = config
+        const getCommand = switchCommand(host)
+        node.log(`Url is ${host}`)
+        //node.log(`Enviroment is ${process.env.NODE_ENV}`)
         node.on('input', function (msg) {
             node.log("Message received, processing");
-            parseJson(msg, node, switchCommand).then(r => {
+            parseJson(msg, host, node, getCommand).then(r => {
                 msg.payload = r
                 node.send(msg);
             })
