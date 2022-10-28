af2bd809f27387d6348dbb3b4a1feb1d7b119f25
Daniel Stahl <danstahl1138@gmail.com>
2022-10-28 17:24:09 -0500

    fixed issues and improved logging

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const tiny = require('tiny-json-http')
 
-//in prod, run on same machine as rest api
-//const URL = process.env.NODE_ENV === "production" ? "http://127.0.0.1:8000" : "http://moode.local:8000"
-const switchCommand = (host) => process.env.NODE_ENV === "test" ? {
+const switchCommand = process.env.NODE_ENV === "test" ? () => ({
     "AdjustVolume": () => Promise.reject(),
     "TurnOn": () => Promise.reject(),
     "TurnOff": () => Promise.reject(),
     "SelectInput": () => Promise.resolve("hi"),
-} : {
+}) : (host) => ({
     "AdjustVolume": async (node, value) => {
         const { volume: volumePercentChange } = value
         const volumeChange = volumePercentChange > 0 ? 5 : -5
         node.log(`Changing volume by ${volumeChange}`);
         try {
-            const { volume } = await tiny.get({ url: `${host}/info` })
-            await tiny.post({ url: `${host}/volume/${volume + volumeChange}` })
+            const info = await tiny.get({ url: `${host}/info` })
+            const { volume } = info.body
+            node.log(`Current volume: ${volume}`)
+            return await tiny.post({ url: `${host}/volume/${volume + volumeChange}` })
         }
         catch (e) {
             node.error(e)
@@ -23,7 +23,7 @@ const switchCommand = (host) => process.env.NODE_ENV === "test" ? {
     "TurnOn": async (node) => {
         node.log("Powering on")
         try {
-            await tiny.post({ url: `${host}/power/on` })
+            return await tiny.post({ url: `${host}/power/on` })
         }
         catch (e) {
             node.error(e)
@@ -32,7 +32,7 @@ const switchCommand = (host) => process.env.NODE_ENV === "test" ? {
     "TurnOff": async (node) => {
         node.log("Powering off")
         try {
-            await tiny.post({ url: `${host}/power/off` })
+            return await tiny.post({ url: `${host}/power/off` })
         }
         catch (e) {
             node.error(e)
@@ -44,13 +44,13 @@ const switchCommand = (host) => process.env.NODE_ENV === "test" ? {
         if (rawInput.startsWith("HDMI")) {
             const [_, input] = rawInput.split(" ")
             try {
-                await tiny.post({ url: `${host}/hdmi/${input}` })
+                return await tiny.post({ url: `${host}/hdmi/${input}` })
             } catch (e) {
                 node.error(e)
             }
         }
     }
-}
+})
 
 const parseJson = (msg, node, switchCommand) => {
     return switchCommand[msg.payload.directive](node, msg.payload.rawDirective.directive.payload)
@@ -60,13 +60,13 @@ module.exports = function (RED) {
     function ParseCommandsToXMC(config) {
         RED.nodes.createNode(this, config);
         const node = this;
-        const host = config
+        const { host } = config
         const getCommand = switchCommand(host)
         node.log(`Url is ${host}`)
         //node.log(`Enviroment is ${process.env.NODE_ENV}`)
         node.on('input', function (msg) {
             node.log("Message received, processing");
-            parseJson(msg, host, node, getCommand).then(r => {
+            parseJson(msg, node, getCommand).then(r => {
                 msg.payload = r
                 node.send(msg);
             })
