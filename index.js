3e32e2afb40691b1bffc9a31995324c50039ea26
Daniel Stahl <danstahl1138@gmail.com>
2022-10-28 14:20:03 -0500

    more logging and bumped

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const tiny = require('tiny-json-http')
 
 //in prod, run on same machine as rest api
 const URL = process.env.NODE_ENV === "production" ? "http://127.0.0.1:8000" : "http://moode.local:8000"
-
 const switchCommand = process.env.NODE_ENV === "test" ? {
     "AdjustVolume": () => Promise.reject(),
     "TurnOn": () => Promise.reject(),
@@ -10,27 +9,45 @@ const switchCommand = process.env.NODE_ENV === "test" ? {
     "SelectInput": () => Promise.resolve("hi"),
 } : {
     "AdjustVolume": async (node, value) => {
-
         const { volume: volumePercentChange } = value
         const volumeChange = volumePercentChange > 0 ? 5 : -5
         node.log(`Changing volume by ${volumeChange}`);
-        const { volume } = await tiny.get(`${URL}/info`)
-        await tiny.post({ url: `${URL}/volume/${volume + volumeChange}` })
+        try {
+            const { volume } = await tiny.get(`${URL}/info`)
+            await tiny.post({ url: `${URL}/volume/${volume + volumeChange}` })
+        }
+        catch (e) {
+            node.error(e)
+        }
     },
     "TurnOn": async (node) => {
         node.log("Powering on")
-        await tiny.post({ url: `${URL}/power/on` })
+        try {
+            await tiny.post({ url: `${URL}/power/on` })
+        }
+        catch (e) {
+            node.error(e)
+        }
     },
     "TurnOff": async (node) => {
         node.log("Powering off")
-        await tiny.post({ url: `${URL}/power/off` })
+        try {
+            await tiny.post({ url: `${URL}/power/off` })
+        }
+        catch (e) {
+            node.error(e)
+        }
     },
     "SelectInput": async (node, value) => {
         const { input: rawInput } = value
+        node.log(`Changing input to ${rawInput}`);
         if (rawInput.startsWith("HDMI")) {
             const [_, input] = rawInput.split(" ")
-            node.log(`Changing input to ${rawInput}`);
-            await tiny.post({ url: `${URL}/hdmi/${input}` })
+            try {
+                await tiny.post({ url: `${URL}/hdmi/${input}` })
+            } catch (e) {
+                node.error(e)
+            }
         }
     }
 }
